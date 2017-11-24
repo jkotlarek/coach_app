083f579b670df2d0e0db75eb82c6c758503f7efe
jkotlarek <jtkotlarek@gmail.com>
2017-11-23 19:34:39 -0800

    Scene Loader Demo

---
+++ CoachApp/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour{
+    
+    public void LoadScene(int scene_id)
+    {
+        SceneManager.LoadScene(scene_id);
+    }
+
+    public void LoadScene(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+}
